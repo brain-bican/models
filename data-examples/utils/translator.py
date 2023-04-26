993203db7eb240d36e270a44c04719014933f244
Sooyoung Ahn <sooyoungka@gmail.com>
2023-04-26 16:56:24 -0700

    added digest, modified id based on feedback, added json schema

--- data-examples/utils/translator.py
+++ data-examples/utils/translator.py
@@ -2,8 +2,11 @@ from models_py.kbmodel import AnnotationCollection
 import yaml
 import csv
 from pathlib import Path
+import requests
+import hashlib
 
 data_files = (Path(__file__).parent / "../source-data")
+data_output = (Path(__file__).parent / "../data.yaml")
 
 # read csv data file into a list of dictionaries of data
 def readCSV(filename):
@@ -18,8 +21,21 @@ annots = readCSV(data_files / '20230412_subset_gene_annotation.csv')
 genomeannots = readCSV(data_files / '20230412_subset_genome_annotation.csv')
 genomeassemblies = readCSV(data_files / '20230412_subset_genome_assembly.csv')
 
+def hashBytestrIter(bytesiter, hasher, ashexstr=False):
+    for block in bytesiter:
+        hasher.update(block)
+    return hasher.hexdigest() if ashexstr else hasher.digest()
+
+def fileAsBlockiter(afile, blocksize=65536):
+    with afile:
+        block = afile.read(blocksize)
+        while len(block) > 0:
+            yield block
+            block = afile.read(blocksize)
+
+
 # map csv to data yaml
-with open(r'../data.yaml', 'w') as file:
+with open(data_output, 'w') as file:
     classannots = dict()
 
     translatedannots = []
@@ -37,12 +53,16 @@ with open(r'../data.yaml', 'w') as file:
     for each in genomeannots:
         translated = dict()
         translated['reference_assembly'] = each['assembly_local_unqiue_identifier']
-        translated['id'] = each['label']
-        translated['in_taxon'] = [each['authority'].upper() + 'Taxon' + ':' + each['taxon_local_unique_identifier']]
+        translated['id'] = 'bican:' + each['label']
+        if each['taxon_identifier_prefix'] == 'taxonomy':
+            translated['in_taxon'] = ['NCBITaxon:' + each['taxon_local_unique_identifier']]
         translated['version'] = each['version']
         translated['description'] = each['description']
         translated['content_url'] = [each['url']]
-        # translated['digest'] = each[''] #checksum
+        r = requests.get(each['url'], allow_redirects=True).content
+        m = hashlib.sha256()
+        m.update(r)
+        translated['digest'] = [m.hexdigest()]
         translatedgenomeannots.append(translated)
     classannots['genome annotations'] = translatedgenomeannots
 
@@ -50,7 +70,8 @@ with open(r'../data.yaml', 'w') as file:
     for each in genomeassemblies:
         translated = dict()
         translated['id'] = each['local_unqiue_identifier']
-        translated['taxon'] = each['taxon_local_unique_identifier']
+        if each['taxon_identifier_prefix'] == 'taxonomy':
+            translated['taxon'] = 'NCBITaxon:' + each['taxon_local_unique_identifier']
         translated['version'] = each['version']
         translated['label'] = each['label']
         translated['description'] = each['description']
@@ -60,7 +81,7 @@ with open(r'../data.yaml', 'w') as file:
     yaml.dump(classannots, file)
 
 # load data yaml
-with open('../data.yaml') as fp:
+with open(data_output) as fp:
     data = yaml.safe_load(fp)
 myannotations = AnnotationCollection(annotations=data.get("annotations"))
 mygenomeannotations = AnnotationCollection(genome_annotations=data.get("genome annotations"))
