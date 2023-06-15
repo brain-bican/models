a36aaa128ef9942d74305952d1fabb67a223d21b
GitHub Action <action@github.com>
2023-06-15 03:45:23 +0000

    generate another formats

---
+++ models_py-autogen/ccn2.py
@@ -0,0 +1,95 @@
+from __future__ import annotations
+from datetime import datetime, date
+from enum import Enum
+from typing import List, Dict, Optional, Any, Union
+from pydantic import BaseModel as BaseModel, Field
+from linkml_runtime.linkml_model import Decimal
+import sys
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+metamodel_version = "None"
+version = "None"
+
+class WeakRefShimBaseModel(BaseModel):
+   __slots__ = '__weakref__'
+
+class ConfiguredBaseModel(WeakRefShimBaseModel,
+                validate_assignment = True,
+                validate_all = True,
+                underscore_attrs_are_private = True,
+                extra = 'forbid',
+                arbitrary_types_allowed = True,
+                use_enum_values = True):
+    pass
+
+
+class Rank(str, Enum):
+    
+    
+    leaf_node = "leaf_node"
+    
+    family = "family"
+    
+    gross = "gross"
+    
+    
+
+class Taxonomy(ConfiguredBaseModel):
+    
+    cell_set_accession: str = Field(..., description="""Primary identifier of the cell set. This field should be programmatically assigned, not edited.""")
+    cell_type_name: Optional[str] = Field(None, description="""The primary name/symbol to be used for the (provisional) cell type defined by this cell set. This is left optional, but is strongly encouraged for every node that is linked.""")
+    parent_cell_set_accession: str = Field(..., description="""The cell set accession of the parent cell set in the taxonomy. This field should be programmatically assigned, not edited.""")
+    synonyms: Optional[str] = Field(None, description="""A list of alternative names for this cell type. Separate entries with a '|'. Do not use terms with a scope that is much narrower or broader than the cell type being described.""")
+    synonym_provenance: Optional[str] = Field(None, description="""Each entry in the synonyms field should have a corresponding entry here, either the DOI of a supporting publication (in the form the form doi:10.1126/journal.abj6641) or the editor's ORCID (in the form: ORCID:01243-234-678). Multiple entries should be separated by a '|'.""")
+    description: Optional[str] = Field(None, description="""Optional free text description of the cluster. This could be particularly useful for describing the properties of cells clustered from techniques that provide data on morphology, function and connectivity, e.g. patch-seq & epi-retro-seq.""")
+    classifying_ontology_term_id: Optional[str] = Field(None, description="""The ID of an ontology term that classifies the cell type defined by this node.""")
+    classifying_ontology_term_name: str = Field(..., description="""The name of the ontology term in the classification_id column""")
+    classification_provenance: str = Field(..., description="""Either the DOI(s) of a supporting publication (in the form the form doi:10.1126/journal.abj6641) or the editor's ORCID (in the form: ORCID:01243-234-678). Multiple entries should be separated by a '|'.""")
+    classification_comment: Optional[str] = Field(None, description="""A free text comment describing the evidence for this classification.""")
+    rank: Optional[Rank] = Field(None, description="""Algorithmically generated hierarchical taxonomies can be complex, with many nodes between root and leaf and branches of variable depth. To simplify this for display and discussion it can be useful to assign nodes to a 3 level hierarchy, with leaf nodes at the bottom.""")
+    
+
+
+class CrossTaxonomyMapping(ConfiguredBaseModel):
+    
+    cell_set_accession: str = Field(..., description="""Primary identifier for cell set.""")
+    cell_type_name: str = Field(..., description="""The primary name/symbol to be used for the cell type defined by this cell set.""")
+    mapped_cell_set_accession: str = Field(..., description="""The accession (ID) of a cell set in a second taxonomy that this cell set maps to.""")
+    mapped_cell_type_name: str = Field(..., description="""The name of the cell type corresponding to the mapped_cell_set_accession.""")
+    evidence_comment: str = Field(..., description="""A free text description of the evidence supporting this mapping. If a similarity_score is include, please also include details of how this was calculated.""")
+    similarity_score: Optional[float] = Field(None, description="""A score recording the similarity between mapped nodes.""", ge=0, le=1)
+    provenance: Optional[str] = Field(None, description="""ORCID of the person doing the mapping using the syntax ORCID:0123-4567-890. Optionally include supporting publications using DOIs of the form doi:10.1126/journal.abj6641.""")
+    
+
+
+class LocationMapping(ConfiguredBaseModel):
+    
+    cell_set_accession: str = Field(..., description="""Primary identifier for cell set.""")
+    cell_type_name: str = Field(..., description="""The primary name/symbol to be used for the cell type defined by this cell set.""")
+    location_ontology_term_id: str = Field(..., description="""The ID of an ontology term that refers to a brain region that this cell type is located in. Ideally this should be the ID of a term defined as a region in a standard atlas.""")
+    location_ontology_term_name: str = Field(..., description="""Name of the term whose ID is recorded in the ontology_term_id field.""")
+    evidence_comment: Optional[str] = Field(None, description="""A comment describing the evidence for this location mapping""")
+    supporting_data: Optional[str] = Field(None, description="""A link to data supporting this location mapping.""")
+    provenance: str = Field(..., description="""ORCID of the person doing the mapping using the syntax ORCID:0123-4567-890. Optionally include supporting publications using DOIs of the form doi:10.1126/journal.abj6641.""")
+    
+
+
+class CellSetAccessionToCellMapping(ConfiguredBaseModel):
+    
+    sample: str = Field(..., description="""Cell sample identifier.""")
+    cell_accessions: List[str] = Field(..., description="""List of cell set accession identifiers.""")
+    
+
+
+
+# Update forward refs
+# see https://pydantic-docs.helpmanual.io/usage/postponed_annotations/
+Taxonomy.update_forward_refs()
+CrossTaxonomyMapping.update_forward_refs()
+LocationMapping.update_forward_refs()
+CellSetAccessionToCellMapping.update_forward_refs()
+
---
+++ models_py-autogen/figure1.py
@@ -0,0 +1,217 @@
+from __future__ import annotations
+from datetime import datetime, date
+from enum import Enum
+from typing import List, Dict, Optional, Any, Union
+from pydantic import BaseModel as BaseModel, Field
+from linkml_runtime.linkml_model import Decimal
+import sys
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+metamodel_version = "None"
+version = "None"
+
+class WeakRefShimBaseModel(BaseModel):
+   __slots__ = '__weakref__'
+
+class ConfiguredBaseModel(WeakRefShimBaseModel,
+                validate_assignment = True,
+                validate_all = True,
+                underscore_attrs_are_private = True,
+                extra = 'forbid',
+                arbitrary_types_allowed = True,
+                use_enum_values = True):
+    pass
+
+
+class CellCategory(str, Enum):
+    
+    
+    NEURON = "NEURON"
+    
+    OTHER_IMN = "OTHER_IMN"
+    
+    OTHER_VASCULAR = "OTHER_VASCULAR"
+    
+    NEUROGLIAL = "NEUROGLIAL"
+    
+    
+
+class BroadRegion(str, Enum):
+    
+    
+    PALL = "PALL"
+    
+    CNU = "CNU"
+    
+    TH = "TH"
+    
+    HY = "HY"
+    
+    MB = "MB"
+    
+    HB = "HB"
+    
+    CB = "CB"
+    
+    
+
+class Division(str, Enum):
+    
+    
+    Pallium_glutamatergic = "Pallium glutamatergic"
+    
+    Subpallium_GABAergic = "Subpallium GABAergic"
+    
+    PALMINUS_SIGNsAMYMINUS_SIGNTHMINUS_SIGNHY_MINUS_SIGNMBMINUS_SIGNHB_neuronal = "PAL−sAMY−TH−HY −MB−HB neuronal"
+    
+    CBXMINUS_SIGNMOBMINUS_SIGNother_neuronal = "CBX−MOB−other neuronal"
+    
+    Neuroglial = "Neuroglial"
+    
+    Vascular = "Vascular"
+    
+    Immune = "Immune"
+    
+    
+
+class NTType(str, Enum):
+    
+    
+    Glut = "Glut"
+    
+    GABA = "GABA"
+    
+    GlutMINUS_SIGNGABA = "Glut−GABA"
+    
+    GABAMINUS_SIGNGlyc = "GABA−Glyc"
+    
+    Chol = "Chol"
+    
+    Dopa = "Dopa"
+    
+    Sero = "Sero"
+    
+    Nora = "Nora"
+    
+    Hist = "Hist"
+    
+    NA = "NA"
+    
+    
+
+class HierarchicalRelationshipType(str, Enum):
+    
+    
+    PARENT_OF = "PARENT_OF"
+    
+    CHILD_OF = "CHILD_OF"
+    
+    
+
+class GroupRelationshipType(str, Enum):
+    
+    
+    CONSISTS_OF = "CONSISTS_OF"
+    
+    MEMBER_OF = "MEMBER_OF"
+    
+    
+
+class NamedThing(ConfiguredBaseModel):
+    
+    id: Optional[str] = Field(None)
+    label: Optional[str] = Field(None)
+    
+
+
+class CellClass(NamedThing):
+    """
+    Class division in Figure 1, also found in Supplementary Materials: Table 7
+    """
+    category: CellCategory = Field(...)
+    has_hierarchical_relationships: Optional[List[HierarchicalRelationship]] = Field(default_factory=list)
+    id: str = Field(...)
+    label: Optional[str] = Field(None)
+    
+
+
+class CellSubclass(NamedThing):
+    """
+    Subclass division in Figure 1
+    """
+    has_hierarchical_relationships: Optional[List[HierarchicalRelationship]] = Field(default_factory=list)
+    division: Optional[Division] = Field(None)
+    nt_type: Optional[NTType] = Field(None)
+    id: str = Field(...)
+    label: Optional[str] = Field(None)
+    
+
+
+class Cluster(NamedThing):
+    """
+    Cluster in Supplementary Materials: Table 7
+    """
+    has_hierarchical_relationships: Optional[List[HierarchicalRelationship]] = Field(default_factory=list)
+    has_group_relationships: Optional[List[GroupRelationship]] = Field(default_factory=list)
+    id: str = Field(...)
+    label: Optional[str] = Field(None)
+    
+
+
+class Cell(NamedThing):
+    """
+    Cell, a member of a cluster
+    """
+    has_group_relationships: Optional[List[GroupRelationship]] = Field(default_factory=list)
+    broad_region: Optional[BroadRegion] = Field(None)
+    id: str = Field(...)
+    label: Optional[str] = Field(None)
+    
+
+
+class Relationship(ConfiguredBaseModel):
+    
+    related_to: str = Field(...)
+    
+
+
+class HierarchicalRelationship(Relationship):
+    
+    relationship_type: Optional[HierarchicalRelationshipType] = Field(None)
+    related_to: str = Field(...)
+    
+
+
+class GroupRelationship(Relationship):
+    
+    relationship_type: Optional[GroupRelationshipType] = Field(None)
+    related_to: str = Field(...)
+    
+
+
+class Container(ConfiguredBaseModel):
+    
+    subclasses: Optional[List[CellSubclass]] = Field(default_factory=list)
+    classes: Optional[List[CellClass]] = Field(default_factory=list)
+    cells: Optional[List[Cell]] = Field(default_factory=list)
+    clusters: Optional[List[Cluster]] = Field(default_factory=list)
+    
+
+
+
+# Update forward refs
+# see https://pydantic-docs.helpmanual.io/usage/postponed_annotations/
+NamedThing.update_forward_refs()
+CellClass.update_forward_refs()
+CellSubclass.update_forward_refs()
+Cluster.update_forward_refs()
+Cell.update_forward_refs()
+Relationship.update_forward_refs()
+HierarchicalRelationship.update_forward_refs()
+GroupRelationship.update_forward_refs()
+Container.update_forward_refs()
+
