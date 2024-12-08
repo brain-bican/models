ce4bc54235ba5ab8e2770b4acf4289155522137d
djarecka <djarecka@gmail.com>
2024-12-08 22:52:36 +0000

    generate another formats

--- models_py-autogen/test_model.py
+++ models_py-autogen/test_model.py
@@ -84,7 +84,7 @@ linkml_meta = LinkMLMeta({'default_prefix': 'bican',
                   'schema': {'prefix_prefix': 'schema',
                              'prefix_reference': 'http://schema.org/'}},
      'source_file': 'test_model.yaml',
-     'title': 'Test Schema updated 4'} )
+     'title': 'Test Schema updated 5'} )
 
 
 class ProvActivity(ConfiguredBaseModel):
